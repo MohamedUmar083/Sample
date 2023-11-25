7824ad881fa16f56e1b57fd33e68a8019dd39917
Mohamed Umar <mohamedumar@Mohameds-MacBook-Air.local>
2023-11-25 11:30:32 +0530

    Nothing

--- add.java
+++ add.java
@@ -1,8 +1,8 @@
 public class add {
     public static void main(String args[])   
 {  
-int n1 = 225, n2 = 115, sum;  
-sum = n1 + n2;  
+int a = 225, b = 115, sum;  
+sum = a + b;  
 System.out.println("The sum of numbers is: "+sum);  
 }  
 }
---
+++ arithmaticCalculator.java
@@ -0,0 +1,16 @@
+public class arithmaticCalculator {
+    public static void main(String args[])   
+{  
+int a = 225, b = 115;
+int add,sub,multi;
+float div;  
+add = a + b;  
+sub = a-b;
+multi = a*b;
+div = a/b;
+System.out.println("The Addition of the numbers is        : " + add);
+System.out.println("The Subraction of the numbers is      : " + sub);
+System.out.println("The Multipliclation of the numbers is : " + multi);
+System.out.println("The Division of the numbers is        : " + div);  
+}  
+}
